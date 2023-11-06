54e2adc00f09625744749e1b4bbb2ae0415d0c75
Tahira <141994477+TahiraD27@users.noreply.github.com>
2023-11-06 10:26:49 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () =>{
+        modalViews.forEach((mv) =>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
 /*=============== SWIPER PRODUCTS ===============*/
 
 
